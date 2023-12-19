143c55cf8029f475bf351c791973fe28b7127647
DisLife373 <111549037+DisLife373@users.noreply.github.com>
2023-12-19 23:00:23 +0700

    Update Profile.jsx can update

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -8,6 +8,10 @@ import './Profile.css';
 export default function Profile() {
     const [deletePopUp, setDeletePopUp] = useState(false);
 
+    const [email_new, setEmail_new]  = useState();
+    const [pass_old, setPass_old]  = useState();
+    const [pass_new, setPass_new]  = useState();
+    const [pass_confirm, setPass_confirm]  = useState();
 
     const [userData, setUserdata] = useState({});
 
@@ -15,16 +19,34 @@ export default function Profile() {
         axios.post('http://localhost:5000/userData', { token: window.localStorage.getItem("token") })
             .then((response) => 
             {
-              setUserdata(response.data.data);
+                setUserdata(response.data.data)
             })  
             .catch(error => console.error(error));
     };
 
+    const HandleSubmit = async (e) => {
+        e.preventDefault();
+        if (pass_new == pass_confirm) {
+            axios.post('http://localhost:5000/updateUser', { 
+                email: userData.email,
+                password: pass_new
+             })
+            .then((response) => {
+                console.log(response.data);
+                setUserdata(response.data.data)
+            })  
+            .catch(error => console.error(error));
+        }
+        else {
+            console.log("error")
+        }
+    }
 
    const logOut=()=>{
         window.localStorage.clear();
         window.location.href="/";
     }
+
     return (
         <div className="wrapper">
             { GetUserData() }
@@ -43,8 +65,7 @@ export default function Profile() {
                             
                             <div className="user-profile-info">
                                 <h3 className="header-text">Edit Profile</h3>
-                                
-                                <form className="profile-container">
+                                <form className="profile-container" onSubmit={(e) => HandleSubmit(e)}>
                                     <div className="picture-container">
                                         <img  className="myprofile" src="https://cdn.discordapp.com/attachments/787359617280770051/1185561360084516884/17.png?ex=65900f0e&is=657d9a0e&hm=2f00dae461fd86c9ca1e2b28e2afa51ec1f628cc08c73f50c84289f8ce2142f2" alt="My Profile" />
                                     </div>
@@ -55,8 +76,8 @@ export default function Profile() {
                                             <input 
                                                 className="input-info"
                                                 name="email" 
-                                                value={userData.email} 
                                                 type="email" 
+                                                value={userData.email}
                                                 disabled
                                             ></input>
                                         </div>
@@ -67,6 +88,7 @@ export default function Profile() {
                                                 name="password" 
                                                 type="password" 
                                                 placeholder="current password" 
+                                                onChange={(e)=> setPass_old(e.target.value)}
                                             ></input>
                                         </div>
                                         <div className="input-box">
@@ -76,6 +98,7 @@ export default function Profile() {
                                                 name="password" 
                                                 type="password" 
                                                 placeholder="new password" 
+                                                onChange={(e)=> setPass_new(e.target.value)}
                                             ></input>
                                         </div>
                                         <div className="input-box">
@@ -85,6 +108,7 @@ export default function Profile() {
                                                 name="password" 
                                                 type="password" 
                                                 placeholder="confirm password" 
+                                                onChange={(e)=> setPass_confirm(e.target.value)}
                                             ></input>
                                         </div>
                                     </div>
@@ -93,16 +117,21 @@ export default function Profile() {
                                             <button 
                                                 className="submit-btn" 
                                                 type="submit" 
+                                                
+                                            >update user</button>
+                                            <button 
+                                                className="submit-btn" 
+                                                type="button" 
                                                 onClick={logOut} 
                                             >log out</button>
-                                            <button 
-                                                className="delete-account-btn" 
-                                                type="button" 
-                                                onClick={() => setDeletePopUp(true)} 
-                                            >delete account</button>
                                         </div>
                                     </div>
                                 </form>
+                                <button 
+                                    className="delete-account-btn" 
+                                    type="button" 
+                                    onClick={() => setDeletePopUp(true)} 
+                                >delete account</button>
                             </div>
                         </div>
                     </div>
